39594112b420a85bc462ba9ff258581e29ebbfc1
caasig1 <caasig1@gmail.com>
2024-07-28 00:19:26 -0400

    helper function

--- app/loading/load_countries.py
+++ app/loading/load_countries.py
@@ -2,10 +2,9 @@ from flask import Flask, render_template, send_file, request, jsonify, session
 from app import app 
 import json, geojson, random, os
 
-@app.route('/countries_na.geojson')
-def countries_na():
+def helper(continent):
     folder = os.path.join(app.static_folder, 'country_outlines')
-    folder = os.path.join(folder, 'na')
+    folder = os.path.join(folder, continent)
     geodata = []
 
     for filename in os.listdir(folder):
@@ -21,23 +20,11 @@ def countries_na():
 
     geodata = geojson.FeatureCollection(geodata)
     return geodata
+
+@app.route('/countries_na.geojson')
+def countries_na():
+    return helper('na')
 
 @app.route('/countries_eu.geojson')
 def countries_eu():
-    folder = os.path.join(app.static_folder, 'country_outlines')
-    folder = os.path.join(folder, 'europe')
-    geodata = []
-
-    for filename in os.listdir(folder):
-        fp = os.path.join(folder, filename)
-        if os.path.isfile(fp):
-            with open(fp, 'r') as f:
-                data = geojson.load(f)
-            
-            if data['type'] == 'FeatureCollection':
-                geodata.extend(data['features'])
-            elif data['type'] == 'Feature':
-                geodata.append(data)
-
-    geodata = geojson.FeatureCollection(geodata)
-    return geodata
+    return helper('eu')
