520e38cad29fb253b294525d6d6645e4113ccab3
caasig1 <caasig1@gmail.com>
2024-07-29 13:51:58 -0400

    typing quizzes keep score

--- app/scorekeeping/score.py
+++ app/scorekeeping/score.py
@@ -18,16 +18,20 @@ def score():
     
     path = os.path.join(app.static_folder, 'score')
     path = os.path.join(path, folder)
+    path = os.path.join(path, type)
     path = os.path.join(path, file)
 
-    with open(path, "r") as f:
-        temp = f.readlines()
-        s = []
-        for entry in temp:
-            add = entry[:-1].split(',')
-            s.append([float(add[0]), float(add[1]), add[2]])
-        s.append([float(score), float(time), date])
-        s = sorted(s, key= lambda x:(-x[0], x[1]))
+    s = []
+    if os.path.isfile(path):
+        with open(path, "r") as f:
+            temp = f.readlines()
+            s = []
+            for entry in temp:
+                add = entry[:-1].split(',')
+                s.append([float(add[0]), float(add[1]), add[2]])
+    s.append([float(score), float(time), date])
+    s = sorted(s, key= lambda x:(-x[0], x[1]))
+    
     
     with open(path, "w") as f:
         for entry in s:
