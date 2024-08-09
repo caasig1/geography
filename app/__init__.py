10e0c6b2522d300c6f1b762f13911c291bf6b788
caasig1 <caasig1@gmail.com>
2024-08-09 17:54:28 -0400

    more imports

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,10 @@ app = Flask(__name__)
 app.secret_key = 'caasig#1'
 
 from app import handler, main_routes
-from app.loading import load_na_subdivisions, load_countries, error_handling
-from app.clicking import countries, subdivisions_na, capitals
-from app.learning import countries, subdivisions_na, capitals
-from app.typing import countries, subdivisions_na, capitals
+from app.loading import load_na_subdivisions, load_countries, error_handling, load_sa_subdivisions
+from app.clicking import countries, subdivisions_na, capitals, subdivisions_sa
+from app.learning import countries, subdivisions_na, capitals, subdivisions_sa
+from app.typing import countries, subdivisions_na, capitals, subdivisions_sa
+from app.travle import load_page
+from app.proximity import load_page
 from app.scorekeeping import score
